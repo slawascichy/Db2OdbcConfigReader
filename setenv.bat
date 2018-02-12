d0d85373a0354c18ec23e7f1e8e102222f361cda
Sławomir 'slawas' Cichy <scichy@ibpm.pro>
2018-02-12 18:23:55 +0100

    Incijalne załadowanie

---
+++ setenv.bat
@@ -0,0 +1,2 @@
+set JAVA_HOME=C:\IBM\DS4.1.1\jdk
+set PATH=%PATH%;C:\IBM\DS4.1.1\jdk\bin;C:\IBM\DS4.1.1\jdk\bin;C:\tools\apache-maven-3.5.2\bin
---
+++ src/test/java/pro/ibpm/gs/db2odbcconfigreader/JdbcConfigReaderTest.java
@@ -0,0 +1,115 @@
+package pro.ibpm.gs.db2odbcconfigreader;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import junit.framework.TestCase;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JdbcConfigReaderTest extends TestCase {
+
+	private static Logger logger = LoggerFactory
+			.getLogger(JdbcConfigReaderTest.class);
+
+	private static final String TEST_PROPERTIES_FILE = "target/test-classes/test.properties";
+	private static final String ODBC_CONF_PROP = "odbc.config.file";
+
+	private Properties readProperties() {
+
+		Properties prop = new Properties();
+		InputStream input = null;
+
+		try {
+			File configFile = new File(TEST_PROPERTIES_FILE);
+			logger.info("configFile={}",
+					new Object[] { configFile.getAbsolutePath() });
+
+			input = new FileInputStream(configFile);
+
+			// load a properties file
+			prop.load(input);
+
+			// get the property value and print it out
+			logger.info(
+					"{}={}",
+					new Object[] { ODBC_CONF_PROP,
+							prop.getProperty(ODBC_CONF_PROP) });
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return prop;
+	}
+
+	public void testReadFile() {
+		JdbcConfigData data;
+		Properties prop = readProperties();
+		JdbcConfigReader reader = JdbcConfigReader.getInstance((String) prop
+				.getProperty(ODBC_CONF_PROP));
+
+		data = reader.getJdbcConfigData("MBRECORD");
+		assertNotNull(data);
+		assertEquals("50000", data.getPort());
+		assertEquals("WARDB11", data.getHostName());
+		assertEquals("MBRECORD", data.getDatabase());
+		assertEquals("MBRECORD", data.getDbAlias());
+		assertEquals("SRV_IIB_DB2_1", data.getUid());
+		assertEquals("SRV_IIB_DB2_1.01", data.getPwd());
+		
+		data = reader.getJdbcConfigData("MBMPDB");
+		assertNotNull(data);
+		assertEquals("50000", data.getPort());
+		assertEquals("WARDB11", data.getHostName());
+		assertEquals("BRK1DBCT", data.getDatabase());
+		assertEquals("BRK1DBCT", data.getDbAlias());
+		assertEquals("SRV_IIB_DB2_1", data.getUid());
+		assertEquals("SRV_IIB_DB2_1.01", data.getPwd());
+
+		data = reader.getJdbcConfigData("BRK1DBCT_PORTAL");
+		assertNotNull(data);
+		assertEquals("50000", data.getPort());
+		assertEquals("WARDB11", data.getHostName());
+		assertEquals("BRK1DBCT", data.getDatabase());
+		assertEquals("BRK1DBCT", data.getDbAlias());
+		assertEquals("SRV_PR_DB2_1", data.getUid());
+		assertEquals("SRV_PR_DB2_1.01", data.getPwd());
+
+		data = reader.getJdbcConfigData("BRK1DBCT_MP");
+		assertNotNull(data);
+		assertEquals("50000", data.getPort());
+		assertEquals("WARDB11", data.getHostName());
+		assertEquals("BRK1DBCT", data.getDatabase());
+		assertEquals("BRK1DBCT", data.getDbAlias());
+		assertEquals("SRV_PM_DB2_1", data.getUid());
+		assertEquals("SRV_PM_DB2_1.01", data.getPwd());
+
+		data = reader.getJdbcConfigData("BRK1DBCT");
+		assertNotNull(data);
+		assertEquals("50000", data.getPort());
+		assertEquals("WARDB11", data.getHostName());
+		assertEquals("BRK1DBCT", data.getDatabase());
+		assertEquals("BRK1DBCT", data.getDbAlias());
+		assertEquals("SRV_IIB_DB2_1", data.getUid());
+		assertEquals("SRV_IIB_DB2_1.01", data.getPwd());
+
+	
+	
+	
+	
+	
+	
+	}
+}
